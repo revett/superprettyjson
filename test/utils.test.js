4cd2823ad6dfb54268ab996633fbabff3fab7116
Charlie Revett <2796074+revett@users.noreply.github.com>
2025-06-13 00:12:28 +0100

    Add tests for utils.js (#4)
    - Setup `avajs/ava`
    - Add GitHub workflow
    - Write tests for `utils.js`

---
+++ test/utils.test.js
@@ -0,0 +1,31 @@
+const test = require("ava");
+const { getMaxIndexLength, indent } = require("../lib/utils.js");
+
+test("getMaxIndexLength: should return 0 for empty object", (t) => {
+  t.is(getMaxIndexLength({}), 0);
+});
+
+test("getMaxIndexLength: should return longest key length", (t) => {
+  const d = {
+    batman: "Bruce Wayne",
+    superman: "Clark Kent",
+    wonderwoman: "Diana Prince",
+  };
+  t.is(getMaxIndexLength(d), 11);
+});
+
+test("indent: should return empty string", (t) => {
+  t.is(indent(0), "");
+});
+
+test("indent: should return single space", (t) => {
+  t.is(indent(1), " ");
+});
+
+test("indent: should return multiple spaces", (t) => {
+  t.is(indent(5), "     ");
+});
+
+test("indent: should handle negative values", (t) => {
+  t.is(indent(-1), "");
+});
