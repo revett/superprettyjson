bc088c4f60e1161f42180a6c2bd24ff2fe7fda12
revett <2796074+revett@users.noreply.github.com>
2025-06-13 00:10:40 +0100

    Order tests correctly

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,6 +1,19 @@
 const test = require("ava");
 const { getMaxIndexLength, indent } = require("../lib/utils.js");
 
+test("getMaxIndexLength: should return 0 for empty object", (t) => {
+  t.is(getMaxIndexLength({}), 0);
+});
+
+test("getMaxIndexLength: should return longest key length", (t) => {
+  const d = {
+    batman: "Bruce Wayne",
+    superman: "Clark Kent",
+    wonderwoman: "Diana Prince",
+  };
+  t.is(getMaxIndexLength(d), 11);
+});
+
 test("indent: should return empty string", (t) => {
   t.is(indent(0), "");
 });
@@ -15,17 +28,4 @@ test("indent: should return multiple spaces", (t) => {
 
 test("indent: should handle negative values", (t) => {
   t.is(indent(-1), "");
-});
-
-test("getMaxIndexLength: should return 0 for empty object", (t) => {
-  t.is(getMaxIndexLength({}), 0);
-});
-
-test("getMaxIndexLength: should return longest key length", (t) => {
-  const d = {
-    batman: "Bruce Wayne",
-    superman: "Clark Kent",
-    wonderwoman: "Diana Prince",
-  };
-  t.is(getMaxIndexLength(d), 11);
 });
