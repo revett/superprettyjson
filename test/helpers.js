bc77f1f93c3204eeb0c167ad463975ff6e37a220
revett <2796074+revett@users.noreply.github.com>
2025-06-15 12:24:02 +0100

    Remove unused function

--- test/helpers.js
+++ test/helpers.js
@@ -16,28 +16,6 @@ const deepEqualMultiline = (t, output, expected) => {
   t.deepEqual(outputLines, expected);
 };
 
-/**
- * Shell out to the CLI programatically within a test, handling if the CLI returns a non-zero exit
- * code.
- * @param {import("ava").Test} t - Ava test instance.
- * @param {string} binPath - Path to the CLI binary.
- * @param {object} options - Execa options to pass to the CLI.
- * @param {string} args - Arguments to pass to the CLI.
- * @returns {string} - Output (stdout) from the CLI.
- */
-const runCLI = async (t, binPath, options, args) => {
-  // Using dynamic import due to CommonJS: https://github.com/sindresorhus/execa/issues/489
-  const { execa } = await import("execa");
-
-  try {
-    const { stdout: output } = await execa(options)`${binPath} ${args}`;
-    return output;
-  } catch (e) {
-    t.fail(e.message);
-  }
-};
-
 module.exports = {
   deepEqualMultiline,
-  runCLI,
 };
